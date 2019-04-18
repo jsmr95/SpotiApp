0336a1e7e7ee9924a63e606b98a36df30a8f7583
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-18 20:13:11 +0200

    Continuamos con la obtención de ultimos lanzamientos y los mostramos

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,11 +12,10 @@ export class SpotifyService {
 
  getNewReleases(){
   const headers = new HttpHeaders({
+    //El token solo dura una hora, habri que volver a generarlo en
+    // https://developer.spotify.com/console/get-new-releases/
     'Authorization': 'Bearer BQCw_u1GINbdTgaJWxU9kqgdn3rb5a9xpFGl0siKrXPVnSjx7jtUes5vfTFuW258C8xnz2chadDCn8uurAwlLACjnRI9Km3kbcth_2abrYgWpulTLLwVe7Nf-VTBKWm_bGu1IpAEHbaBqAU',
   })
-  this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
-    .subscribe(data => {
-      console.log(data);
-    });
+  return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
  }
 }
