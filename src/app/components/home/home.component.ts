b1814c79a34676101448e41b71119567888f13ba
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-18 17:32:39 +0200

    Introducción a peticiones HTTP

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  paises: any[] = [];
+  constructor( private http: HttpClient) {
+    //Hago una petición get
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .subscribe( (data: any) => {
+        this.paises = data;
+      });
+  }
 
   ngOnInit() {
   }
