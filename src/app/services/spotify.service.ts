c1ce323b9f81e02df52c1330fdb0fcab1da882b3
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-21 10:15:50 +0200

    Mas optimizaciones

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,23 +12,22 @@ export class SpotifyService {
    console.log('Spotify service listo!')
  }
 
- getNewReleases(){
-  const headers = new HttpHeaders({
-    //El token solo dura una hora, habri que volver a generarlo en
-    // https://developer.spotify.com/console/get-new-releases/
-    'Authorization': 'Bearer BQDuUe3rf3V6n2_e-E8nDd15CSgdiZwcV19DftVAhxHcXt87ITIHC0LFWWj0Y6raSSqmDtkRrVOWVASG86GLYLEouHE-_j-8nAFo6eUvHRQEytXc5BI6nrq7tbJD3o3RtHJEkeSARcJKnaE',
-  })
-  return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
-    .pipe( map( data => data['albums'].items ) );
- }
-
- getArtista (termino: string){
+ getQuery(query:string){
+   const url = `https://api.spotify.com/v1/${query}`;
    const headers = new HttpHeaders({
      //El token solo dura una hora, habri que volver a generarlo en
      // https://developer.spotify.com/console/get-new-releases/
      'Authorization': 'Bearer BQDuUe3rf3V6n2_e-E8nDd15CSgdiZwcV19DftVAhxHcXt87ITIHC0LFWWj0Y6raSSqmDtkRrVOWVASG86GLYLEouHE-_j-8nAFo6eUvHRQEytXc5BI6nrq7tbJD3o3RtHJEkeSARcJKnaE',
-   })
-   return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
+   });
+   return this.http.get(url, {headers});
+ }
+
+ getNewReleases(){
+  return this.getQuery('browse/new-releases').pipe( map( data => data['albums'].items ) );
+ }
+
+ getArtista (termino: string){
+   return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
    .pipe( map( data => data['artists'].items  ) );
  }
 }
