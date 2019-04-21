77e6c68649a50a3d4d7d6af925692c5e1050c69d
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-21 11:30:23 +0200

    Creo la vista de cada artista

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {SpotifyService} from '../../services/spotify.service';
 
 
 @Component({
@@ -7,15 +8,24 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './artista.component.html',
   styles: []
 })
-export class ArtistaComponent implements OnInit {
+export class ArtistaComponent {
 
-  constructor( private router: ActivatedRoute) {
+  artista: any = {};
+  loading:boolean;
+  constructor( private router: ActivatedRoute,
+               private spotify: SpotifyService) {
+    this.loading = true;
     this.router.params.subscribe( params => {
-      
+      this.getArtista(params['id']);
     });
    }
 
-  ngOnInit() {
+  getArtista (id:string){
+    this.spotify.getArtista(id)
+      .subscribe( artista => {
+        this.artista = artista;
+        this.loading = false;
+      });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -26,8 +26,14 @@ export class SpotifyService {
   return this.getQuery('browse/new-releases').pipe( map( data => data['albums'].items ) );
  }
 
- getArtista (termino: string){
+ getArtistas (termino: string){
    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
    .pipe( map( data => data['artists'].items  ) );
  }
+
+ getArtista (id: string){
+   return this.getQuery(`artists/${id}`);
+   // .pipe( map( data => data['artists'].items  ) ); Ya viene la info transformada, no hay que pasarla por el pipe
+ }
+
 }
